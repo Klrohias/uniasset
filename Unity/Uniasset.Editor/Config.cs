3e587aefb2ca127b330047071014977f0358ba74
qingyiwebt <qingyiweb@gmail.com>
2026-06-06 14:42:34 +0800

    chore: bump to v0.2.0-rc9

--- Unity/Uniasset.Editor/Config.cs
+++ Unity/Uniasset.Editor/Config.cs
@@ -3,6 +3,6 @@ namespace Uniasset.Editor
     public static class Config
     {
         public const string GitHubRepoURL = "https://github.com/Klrohias/uniasset";
-        public const string Version = "v0.2.0-rc8";
+        public const string Version = "v0.2.0-rc9";
     }
 }
