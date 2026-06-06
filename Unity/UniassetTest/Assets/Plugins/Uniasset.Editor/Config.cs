c800380f37cea3ea8a6cec0cae9e5ef8da4dc763
qingyiwebt <qingyiweb@gmail.com>
2026-06-06 14:40:57 +0800

    refactor: use symbolic link instead file for `Uniasset.Editor`

--- Unity/UniassetTest/Assets/Plugins/Uniasset.Editor/Config.cs
+++
@@ -1,8 +0,0 @@
-namespace Uniasset.Editor
-{
-    public static class Config
-    {
-        public const string GitHubRepoURL = "https://github.com/Klrohias/uniasset";
-        public const string Version = "v0.2.0-rc7";
-    }
-}
